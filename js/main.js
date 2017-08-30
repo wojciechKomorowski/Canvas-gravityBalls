11fe84b769e786f63bb4c1bd385dcbb2c5a2c1e3
Wojciech Komorowski <komora89@gmail.com>
2017-08-30 23:09:43 +0200

    Removed stroke

--- js/main.js
+++ js/main.js
@@ -76,7 +76,6 @@ function Ball(x, y, dx, dy, radius, color) {
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
-        c.stroke();
         c.closePath();
     };
 }
